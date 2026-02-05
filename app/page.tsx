cf1ea393b76c4db024b76474510be4d821621831
Anna Sydorova <anna.sydorova.job@gmail.com>
2026-02-05 14:30:49 +0200

    feat: add navbar and sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <main>hiiii</main>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:text-gray-100">
+      hiiii
+    </main>
   );
 }
